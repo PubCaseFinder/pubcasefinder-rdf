01c7cf5f9e450b676995d15e370c5094c5c496bc
hmotoyoshi-coder <h.motoyoshi@trinet.co.jp>
2026-06-24 13:21:09 +0900

    [fix] HPOの古い翻訳をlatestディレクトリにセットするスクリプトを追加

--- create_latest_directory.sh
+++ create_latest_directory.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# latestディレクトリの作成
 for dir in `ls -d data/source/*/`; do
     dir_name=${dir}/$(date '+%Y%m%d')
     mkdir ${dir_name}
     (cd ${dir} && ln -snf ${dir_name} latest)
 done
+
+# HPOの旧翻訳をlatestにコピーする
+old_dir=`ls -r data/source/HPO | sed -E 's@[^0-9]@@g' | grep -v '^$' | grep -v $(date '+%Y%m%d') | head -n 1`
+cp data/source/HPO/${old_dir}/HPO_Inheritance_en_jp.txt data/source/HPO/latest/
