59a7c55d1bd565e77b1ce4b3cdbd2b1bf834f7cc
hmotoyoshi-coder <h.motoyoshi@trinet.co.jp>
2026-06-24 15:42:04 +0900

    [fix] rdfのlatestディレクトリ作成

--- create_latest_directory.sh
+++ create_latest_directory.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# latestディレクトリの作成
+# source latestディレクトリの作成
 for dir in `ls -d data/source/*/`; do
     dir_name=${dir}/$(date '+%Y%m%d')
     mkdir -p ${dir_name}
     (cd ${dir} && ln -snf $(date '+%Y%m%d') latest)
 done
+
+# rdf latestディレクトリの作成
+(
+    cd data/rdf/;
+    mkdir $(date '+%Y%m%d');
+    ln -snf "$(date '+%Y%m%d')/" latest
+)
 
 # HPOの旧翻訳をlatestにコピーする
 old_dir=`ls -r data/source/HPO | sed -E 's@[^0-9]@@g' | grep -v '^$' | grep -v $(date '+%Y%m%d') | head -n 1`
